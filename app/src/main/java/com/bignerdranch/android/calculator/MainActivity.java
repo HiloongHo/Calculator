25d7c01de3a2ea8db2d64b23bcbdad2f7ced6115
HiloongHo <hiloongho@gmail.com>
2024-12-15 01:56:51 +0800

    首次提交

--- app/src/main/java/com/bignerdranch/android/calculator/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/calculator/MainActivity.java
@@ -59,8 +59,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     void assignID(MaterialButton btn, int id) {
-        btn = findViewById(id);
-        btn.setOnClickListener(this);
+        MaterialButton button = findViewById(id);
+        button.setOnClickListener(this);
     }
 
     @Override
@@ -74,14 +74,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             resultTv.setText("0");
             return;
         }
+        
         if (buttonText.equals("=")) {
-            solutionTv.setText(resultTv.getText());
+            String finalResult = getResult(dataToCalculate);
+            if (!finalResult.equals("Err")) {
+                solutionTv.setText(finalResult);
+                resultTv.setText(finalResult);
+            }
+            return;
         }
+        
         if (buttonText.equals("C")) {
-            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
-        }else {
-            dataToCalculate = dataToCalculate + buttonText;
+            if (dataToCalculate.length() > 0) {
+                dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+                solutionTv.setText(dataToCalculate);
+                if (dataToCalculate.length() > 0) {
+                    String finalResult = getResult(dataToCalculate);
+                    if (!finalResult.equals("Err")) {
+                        resultTv.setText(finalResult);
+                    }
+                } else {
+                    resultTv.setText("0");
+                }
+            }
+            return;
         }
+
+        if (buttonText.equals("×")) {
+            buttonText = "*";
+        } else if (buttonText.equals("÷")) {
+            buttonText = "/";
+        }
+
+        if (dataToCalculate.equals("") && isOperator(buttonText)) {
+            return;
+        }
+
+        if (dataToCalculate.length() > 0 && isOperator(buttonText) && 
+            isOperator(String.valueOf(dataToCalculate.charAt(dataToCalculate.length() - 1)))) {
+            return;
+        }
+
+        dataToCalculate = dataToCalculate + buttonText;
         solutionTv.setText(dataToCalculate);
 
         String finalResult = getResult(dataToCalculate);
@@ -89,19 +123,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             resultTv.setText(finalResult);
         }
     }
+
+    private boolean isOperator(String str) {
+        return str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/");
+    }
+
     String getResult(String data) {
         try {
+            if (data.isEmpty()) return "0";
+            
+            if (isOperator(String.valueOf(data.charAt(data.length() - 1)))) {
+                data = data.substring(0, data.length() - 1);
+            }
+            
             Context context = Context.enter();
             context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initStandardObjects();
             String finalResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+            
             if (finalResult.endsWith(".0")) {
                 finalResult = finalResult.replace(".0", "");
             }
+            
+            if (finalResult.equals("Infinity") || finalResult.equals("-Infinity")) {
+                return "Err";
+            }
+            
             return finalResult;
         } catch (Exception e) {
             return "Err";
+        } finally {
+            Context.exit();
         }
-
     }
 }
